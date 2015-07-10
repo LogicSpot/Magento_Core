972e55dbfe8c0b7965f975b2a3de7eb5255c277a
Kamil <kamil@logicspot.com>
2015-07-10 12:35:36 +0100

    move js constant BASE_URL from imageload module into Core, to enable it for other LogicSpot modules

--- app/code/community/LogicSpot/Core/Model/Design/Package.php
+++ app/code/community/LogicSpot/Core/Model/Design/Package.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * LogicSpot_Core
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GNU General Public License v3.0
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * @category    LogicSpot
+ * @package     LogicSpot_Core
+ * @copyright   Copyright (c) 2015 LogicSpot (http://www.logicspot.com)
+ * @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License v3.0
+ */
+
 class LogicSpot_Core_Model_Design_Package extends Mage_Core_Model_Design_Package
 {
 	/**
